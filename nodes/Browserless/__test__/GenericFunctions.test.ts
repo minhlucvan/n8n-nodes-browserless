7ee624be1215e9d73c91cc8a282af19b78daac5f
Minh Luc Van <luk.mink@gmail.com>
2022-11-20 11:59:21 +0700

    implement test

---
+++ nodes/Browserless/__test__/GenericFunctions.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from '@jest/globals';
+import { browserlessPageOptionsFileds } from '../BrowserlessDescriptions';
+import { content } from '../chemas/browserless-api.schema';
+import { parseCollectionOptions } from '../GenericFunctions'
+
+describe('parseFixedCollectionOptions', () => {
+	it('should parse cookies as an array', () => {
+		const collectionOptions = {
+			cookies: { cookies: [{ name: 'Test', value: 'test' }] },
+		};
+		const parsedFixedCollectionOptions = parseCollectionOptions(browserlessPageOptionsFileds, collectionOptions);
+		console.log('----', parsedFixedCollectionOptions);
+		const results = content.validate({
+			url:	'https://google.com',
+			...parsedFixedCollectionOptions,
+		})
+		// console.log(results);
+		expect(results.error).toBeUndefined();
+	})
+
+	it('should parse header as an object', () => {
+		const collectionOptions = {
+			setExtraHTTPHeaders: { header: { name: 'Test', value: 'test' } },
+		};
+		const parsedFixedCollectionOptions = parseCollectionOptions(browserlessPageOptionsFileds, collectionOptions);
+		console.log('----', parsedFixedCollectionOptions);
+		const results = content.validate({
+			url:	'https://google.com',
+			...parsedFixedCollectionOptions,
+		})
+		// console.log(results);
+		expect(results.error).toBeUndefined();
+	})
+	it('should parse authenticate properly', () => {
+		const collectionOptions = {
+			authenticate: { authenticate: { usename: 'Test', password: 'test' } },
+		};
+		const parsedFixedCollectionOptions = parseCollectionOptions(browserlessPageOptionsFileds, collectionOptions);
+		console.log('----', parsedFixedCollectionOptions);
+		const results = content.validate({
+			url:	'https://google.com',
+			...parsedFixedCollectionOptions,
+		})
+		// console.log(results);
+		expect(results.error).toBeUndefined();
+	})
+});
