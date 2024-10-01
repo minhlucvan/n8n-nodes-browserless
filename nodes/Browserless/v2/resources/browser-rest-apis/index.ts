98da8e37ec9f48c2aa78ec08a6da809e089cfd0d
Luc Van Minh <luk.mink@gmail.com>
2024-10-01 13:00:21 +0700

    debug ref

--- nodes/Browserless/v2/resources/browser-rest-apis/index.ts
+++ nodes/Browserless/v2/resources/browser-rest-apis/index.ts
@@ -7,49 +7,9 @@
 import { INodeProperties, INodePropertyOptions } from 'n8n-workflow'
 import { runHooks } from './hooks'
 
-import * as chromeUnblock from './chrome-unblock'
-import * as chromiumUnblock from './chromium-unblock'
-import * as chromiumPdf from './chromium-pdf'
-import * as chromiumScreenshot from './chromium-screenshot'
-import * as chromeContent from './chrome-content'
-import * as chromeDownload from './chrome-download'
-import * as chromeFunction from './chrome-function'
-import * as jsonList from './json-list'
-import * as jsonNew from './json-new'
-import * as jsonProtocol from './json-protocol'
-import * as jsonVersion from './json-version'
-import * as chromePdf from './chrome-pdf'
-import * as chromePerformance from './chrome-performance'
 import * as chromeScrape from './chrome-scrape'
-import * as chromeScreenshot from './chrome-screenshot'
-import * as chromiumContent from './chromium-content'
-import * as chromiumDownload from './chromium-download'
-import * as chromiumFunction from './chromium-function'
-import * as chromiumPerformance from './chromium-performance'
-import * as chromiumScrape from './chromium-scrape'
 
-const operations: INodePropertyOptions[] = [
-  chromeUnblock.option,
-  chromiumUnblock.option,
-  chromiumPdf.option,
-  chromiumScreenshot.option,
-  chromeContent.option,
-  chromeDownload.option,
-  chromeFunction.option,
-  jsonList.option,
-  jsonNew.option,
-  jsonProtocol.option,
-  jsonVersion.option,
-  chromePdf.option,
-  chromePerformance.option,
-  chromeScrape.option,
-  chromeScreenshot.option,
-  chromiumContent.option,
-  chromiumDownload.option,
-  chromiumFunction.option,
-  chromiumPerformance.option,
-  chromiumScrape.option,
-]
+const operations: INodePropertyOptions[] = [chromeScrape.option]
 
 export const name = 'Browser Rest Apis'
 
@@ -74,26 +34,7 @@ operationSelect.default = operations.length > 0 ? operations[0].value : ''
 
 export const rawProperties: INodeProperties[] = [
   operationSelect,
-  ...chromeUnblock.properties,
-  ...chromiumUnblock.properties,
-  ...chromiumPdf.properties,
-  ...chromiumScreenshot.properties,
-  ...chromeContent.properties,
-  ...chromeDownload.properties,
-  ...chromeFunction.properties,
-  ...jsonList.properties,
-  ...jsonNew.properties,
-  ...jsonProtocol.properties,
-  ...jsonVersion.properties,
-  ...chromePdf.properties,
-  ...chromePerformance.properties,
   ...chromeScrape.properties,
-  ...chromeScreenshot.properties,
-  ...chromiumContent.properties,
-  ...chromiumDownload.properties,
-  ...chromiumFunction.properties,
-  ...chromiumPerformance.properties,
-  ...chromiumScrape.properties,
 ]
 
 const { properties, methods } = runHooks(rawProperties)
